d134ed85d3ac8d4b5a575c43f62eb4222412656e
Fábio Ghetti <fabioghetti@hotmail.com>
2018-10-01 23:27:37 -0300

    validações ao realizar a conversão de um objeto para um DTO.

--- controleProdutos/src/main/java/com/ghetti/fabio/controle/produto/converter/ProdutoConverter.java
+++ controleProdutos/src/main/java/com/ghetti/fabio/controle/produto/converter/ProdutoConverter.java
@@ -9,11 +9,13 @@ import com.ghetti.fabio.controle.produto.model.Produto;
 public class ProdutoConverter {
 
 	public static ProdutoTO convertToTransferObject(Produto produto) {
+		if (produto == null) return null;
 		return new ProdutoTO(produto.getDescricao(), produto.getFornecedor(), 
 				produto.getSetor(), produto.getQuantidade(), produto.getValor());
 	}
 	
 	public static List<ProdutoTO> convertToTransferObjectList(List<Produto> produtos) {
+		if (produtos == null || produtos.isEmpty()) return null;
 		List<ProdutoTO> produtosTO = new ArrayList<ProdutoTO>();
 		for (Produto produto : produtos) {
 			produtosTO.add(ProdutoConverter.convertToTransferObject(produto));
