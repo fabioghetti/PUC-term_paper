4c38aa066279652f4dfdfa97773fbdc2538ac2cd
Fábio Ghetti <fabioghetti@hotmail.com>
2018-10-01 23:34:54 -0300

    ajustes no API Gateway.

--- controleProdutos/src/main/java/com/ghetti/fabio/controle/produto/converter/VendaConverter.java
+++ controleProdutos/src/main/java/com/ghetti/fabio/controle/produto/converter/VendaConverter.java
@@ -6,6 +6,7 @@ import com.ghetti.fabio.controle.produto.model.Venda;
 public class VendaConverter {
 
 	public static VendaTO convertToTransferObject(Venda venda) {
+		if (venda == null) return null;
 		return new VendaTO(venda.getCliente(), venda.getData(), venda.getStatus(), venda.getNumeroNotaFiscal(), venda.getDesconto(), 
 				venda.getCupom(), venda.getPromocao(), venda.getVendedorExterno(), venda.getFormaPagamento(), venda.getDataEntrega(),
 				venda.getTipoEntrega(), ProdutoConverter.convertToTransferObjectList(venda.getProdutos()));
